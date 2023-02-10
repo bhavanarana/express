cfe1afb561ffc98cf253b5368df60048946e9ec3
Bhavana <4bhavanarana@gmail.com>
2023-02-10 13:49:50 +0530

    Sending data to the server

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const express = require("express"); //fetch lib
+const express = require("express");
 const path = require("path");
-const port = 8001;
+const port = 8000;
+
 const app = express();
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.get("/", (req, res) => {
-  //at which this index.js file server is started
-  // console.log(__dirname);
-  // res.send("<h1>ahaaan you gotchaa!</h1>");
-  return res.render("home", { title: "My Contact List" });
+
+var contactList = [
+  {
+    name: "Arpan",
+    phone: "1111111111",
+  },
+  {
+    name: "Tony Stark",
+    phone: "1234567890",
+  },
+  {
+    name: "Coding Ninjas",
+    phone: "12131321321",
+  },
+];
+//controllers
+app.get("/", function (req, res) {
+  //render finds an ejs file and render it
+  return res.render("home", {
+    title: "Contact List",
+    contact_list: contactList,
+  });
 });
 app.get("/practice", (req, res) => {
   return res.render("practice", {
-    title: "let us play with ejs",
+    title: "let's do it",
   });
 });
-app.listen(port, (err) => {
+
+app.post("/create-contact", (req, res) => {
+  //take me to tht url or route
+  return res.redirect("/practice");
+});
+
+app.listen(port, function (err) {
   if (err) {
-    console.log("error in running the server", err);
+    console.log("Error in running the server", err);
   }
-  console.log("working");
+  console.log("Yup!My Server is running on Port", port);
 });
